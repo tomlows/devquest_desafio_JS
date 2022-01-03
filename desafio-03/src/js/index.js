2075202de352cb0abd5bf0506687ab563601f987
unknown <tomlopws45@gmail.com>
2022-01-03 18:43:54 -0300

    first commit

---
+++ desafio-03/src/js/index.js
@@ -0,0 +1,9 @@
+
+let mensagemOpcoes = confirm('Seja bem-vindo(a), Aventureiro(a)! 
+Gostaria de pagar R$50,00 para passar a noite na nosso hospedagem?')
+
+if (mensagemOpcoes == true) {
+    alert('Ótimo! Nós temos as melhores camas de toda a região!')
+} else {
+    alert('Que pena! Você parecia ser uma pessoa mais legal.')
+}
